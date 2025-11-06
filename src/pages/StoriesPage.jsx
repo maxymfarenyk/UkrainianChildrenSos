8955fa0166c7e50dfaab4eb7dc0657f9dca05ca3
maxymfarenyk <maxfar2004@gmail.com>
2025-11-06 16:08:48 +0200

    Added description section for StoriesPage and fixed some visual bugs

--- src/pages/StoriesPage.jsx
+++ src/pages/StoriesPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import HelpBanner from "../components/HelpBanner/HelpBanner.jsx";
 import StoriesHero from "../sections/Stories/Hero/StoriesHero.jsx";
+import StoriesDescription from "../sections/Stories/Description/StoriesDescription.jsx";
 
 const StoriesPage = () => {
     return (
         <main>
             <StoriesHero />
+            <StoriesDescription />
             <HelpBanner />
         </main>
     );
---
+++ src/sections/Stories/Description/StoriesDescription.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import '../../Fund/Description/FundDescription.css'
+import { useLanguage } from '../../../context/LanguageContext.jsx';
+
+const Description = () => {
+    const { t } = useLanguage();
+
+    return (
+        <section className="fund-description">
+            <div className="fund-description__inner">
+                <div className="fund-description__content">
+                    <h2 className="fund-description__title">
+                        {t('stories.description.title')}
+                    </h2>
+                    <div className="fund-description__text-grid">
+
+                        <div className="fund-description__column fund-description__column--left">
+                            <p dangerouslySetInnerHTML={{ __html:t('stories.description.p1')}}/>
+                        </div>
+
+                        <div className="fund-description__column fund-description__column--right">
+                            <p>{t('stories.description.p2')}</p>
+                        </div>
+                    </div>
+                    <div className="description-photo">
+                        <img src='/assets/photos/UN0770704.jpg 1.png' alt='Чоловік з дитиною на фоні руїн будинку'></img>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Description;
